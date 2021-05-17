f3e90895ce4730a284d53a2081686336fe4a44b1
Sarah Rudder <sarah.rudder@gsa.gov>
2021-05-17 12:58:53 -0500

    Add more of footer, fine tune eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,13 +16,36 @@ module.exports = {
   // add your custom rules here
   rules: {
     "comma-dangle": 0,
-    "semi": ["off"],
-    "quotes": ["off"],
-    "indent": ["off"],
+    semi: ["off"],
+    quotes: ["off"],
+    indent: ["off"],
     "padded-blocks": ["off"],
     "vue/singleline-html-element-content-newline": 0,
     "vue/html-indent": 0,
     "vue/html-closing-bracket-spacing": 0,
     "vue/no-v-html": 0,
+    "vue/max-attributes-per-line": ["error", {
+      singleline: {
+        max: 3,
+        allowFirstLine: true
+      },
+      multiline: {
+        max: 3,
+        allowFirstLine: true
+      }
+    }],
+    "vue/html-closing-bracket-newline": ["error", {
+      singleline: "never",
+      multiline: "never"
+    }],
+    "vue/html-self-closing": ["error", {
+      html: {
+        void: "any",
+        normal: "any",
+        component: "any"
+      },
+      svg: "always",
+      math: "always"
+    }]
   }
 }
