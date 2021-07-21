d256b3e37b0511eb1b20abcb530079ac9d564053
Taylor Zajicek <tzajicek@flexion.us>
2021-07-21 10:35:10 -0500

    Remove alert heading

--- components/Alert.vue
+++ components/Alert.vue
@@ -2,7 +2,7 @@
     <section class="usa-site-alert usa-site-alert--info margin-bottom-2" aria-label="Site alert,">
         <div class="usa-alert">
             <div class="usa-alert__body">
-                <h3 class="usa-alert__heading">{{ heading }}</h3>
+                <h3 v-if="heading" class="usa-alert__heading">{{ heading }}</h3>
                 <p class="usa-alert__text">
                     <slot></slot>
                 </p>
@@ -14,10 +14,10 @@
 <script>
 export default {
     props: {
+        // eslint-disable-next-line vue/require-default-prop
         heading: {
             type: String,
-            required: true,
-            default: 'Alert heading not provided',
+            required: false,
         },
     },
 };
