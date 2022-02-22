baf8aa62864c66ec3cb0ce197ca631b608392f91
Luke Garceau <lgarceau@uncc.edu>
2022-02-22 13:24:18 -0500

    Created a test for Checkbox.vue

--- test/Components/Checkbox.spec.js
+++ test/Components/Checkbox.spec.js
@@ -1,28 +1,27 @@
-import { mount, shallowMount } from "@vue/test-utils"
-import { Store } from "vuex"
-import CheckBox from "@/components/CheckBox.vue"
-import beforeAllTests from "@/test/beforeAllTests"
+import { mount, shallowMount } from '@vue/test-utils'
+import Checkbox from '@/components/Checkbox.vue'
+import beforeAllTests from '@/test/beforeAllTests'
 import { state as criteriaState, mutations, getters } from "~/store/criteria"
+import { Store } from 'vuex'
 
-const MOCK_CRITERIA = {
-  criteriaKey: "deceased_served_in_active_military",
-  label:
-    "The deceased served in the active military, naval, or air service and",
-  type: "boolean",
-  response: false,
-  location: "benefit-card"
+const CRITERIA_KEY = 'deceased_buried_in_unmarked_grave'
+const ID = `checkbox-1-${CRITERIA_KEY}`
+const PROPS_DATA = {
+  criteriaKey: CRITERIA_KEY,
+  label: 'Testing Checkbox Label',
+  response: 'test',
+  location: 'benefit-card'
 }
 
-describe("<CheckBox/>", () => {
-  let store
-  let actions
-
+describe('Checkbox', () => {
+  let store, actions
   beforeAll(async () => {
     await beforeAllTests()
   })
 
   beforeEach(() => {
     criteriaState.namespaced = true
+
     actions = {
       updateResponse: jest.fn()
     }
@@ -40,55 +39,46 @@ describe("<CheckBox/>", () => {
     })
   })
 
-  test("is a Vue instance", () => {
-    const wrapper = mount(CheckBox, {
-      propsData: {
-        criteriaKey: MOCK_CRITERIA.criteriaKey,
-        label: MOCK_CRITERIA.label,
-        response: MOCK_CRITERIA.response
-      }
+
+  test('is a Vue instance', () => {
+    const wrapper = mount(Checkbox, {
+      propsData: PROPS_DATA
     })
     expect(wrapper.vm).toBeTruthy()
   })
 
-  test("displays eligibilityCriteria when one is passed in", () => {
-    const wrapper = shallowMount(CheckBox, {
-      propsData: { ...MOCK_CRITERIA },
-      store
-    })
-    expect(wrapper.find("label").text()).toBe(
-      "The deceased served in the active military, naval, or air service and"
-    )
-    expect(
-      wrapper.find(".usa-checkbox__input").element.checked
-    ).not.toBeTruthy()
+  test('displays with no props', () => {
+    const wrapper = mount(Checkbox)
+    expect(wrapper.vm).toBeTruthy()
   })
 
-  test("updates when a checkbox criteria response changes", async () => {
-    const wrapper = shallowMount(CheckBox, {
-      propsData: { ...MOCK_CRITERIA },
-      store
+  test('location (benefit-card) returns different selected style', () => {
+    const wrapper = mount(Checkbox, {
+      propsData: {
+        ...PROPS_DATA,
+        location: 'benefit-card',
+        response: true
+      }
     })
-    await wrapper.find(".usa-checkbox__input").setChecked()
-    expect(wrapper.find(".usa-checkbox__input").element.checked).toBeTruthy()
+    expect(wrapper.vm.selectedStyle).toBe('text-success text-bold')
+  })
+
+  test('location (left-rail) returns different selected style', () => {
+    const wrapper = mount(Checkbox, {
+      propsData: {
+        ...PROPS_DATA,
+        location: 'left-rail',
+        response: true
+      }
+    })
+    expect(wrapper.vm.selectedStyle).toBe(null)
+  })
+
+  test('clicking checkbox results in update in store', async () => {
+    const wrapper = shallowMount(Checkbox, { propsData: PROPS_DATA, store})
+    const checkboxInput = wrapper.find("input")
+    await checkboxInput.setChecked()
     expect(actions.updateResponse).toHaveBeenCalled()
   })
-  test("when checkbox is selected it must have correct styling", async () => {
-    const MOCK_CHECKBOX_SELECTED = {
-      criteriaKey: "deceased_served_in_active_military",
-      label:
-        "The deceased served in the active military, naval, or air service and",
-      type: "boolean",
-      response: true,
-      location: "benefit-card"
-    }
 
-    const wrapper = shallowMount(CheckBox, {
-      propsData: { ...MOCK_CHECKBOX_SELECTED },
-      store
-    })
-    await wrapper.find(".usa-checkbox__input").setChecked()
-    expect(wrapper.find(".usa-checkbox__input").element.checked).toBeTruthy()
-    expect(wrapper.find("label").classes()).toContain("text-success")
-  })
 })
