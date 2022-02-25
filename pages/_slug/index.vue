d4349f7b58e388e4e4896624137d41547e43aa5e
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-02-25 11:38:25 -0600

    wip

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -3,27 +3,18 @@
     <section class="grid-container">
       <div class="grid-row grid-gap">
         <div class="tablet:grid-col">
-          <h1
-            v-if="lifeEventTitle"
-            class="font-heading-lg tablet:font-heading-xl margin-top-6 text-secondary"
-          >
+          <h1 v-if="lifeEventTitle" class="font-heading-lg tablet:font-heading-xl margin-top-6 text-secondary">
             {{ lifeEvent.secondaryHeadline }}
           </h1>
-          <p
-            v-if="lifeEvent.lede"
-            class="tablet:font-heading-lg line-height-serif-6 text-normal measure-6"
-          >
+          <p v-if="lifeEvent.lede" class="tablet:font-heading-lg line-height-serif-6 text-normal measure-6">
             {{ lifeEvent.lede }}
           </p>
           <ol class="usa-process-list">
             <li
               v-for="step in landingPage.processListSteps"
               :key="step"
-              class="usa-process-list__item padding-bottom-4"
-            >
-              <p
-                class="usa-process-list__heading font-sans-md tablet:font-sans-lg line-height-sans-1"
-              >
+              class="usa-process-list__item padding-bottom-4">
+              <p class="usa-process-list__heading font-sans-md tablet:font-sans-lg line-height-sans-1">
                 {{ step }}
               </p>
             </li>
@@ -33,80 +24,54 @@
 
       <div role="complementary" class="grid-row grid-gap print:display-none">
         <div
-          class="display-none tablet:display-block tablet:grid-col-5 desktop:grid-col-4 margin-y-2 print:display-none"
-        ></div>
-        <div
-          class="tablet:grid-col-4 desktop:grid-col-3 display-flex flex-align-center margin-y-2 print:display-none"
-        >
-          <div>
-            <button
-              class="usa-button usa-button--unstyled open-all"
-              aria-controls="acc-id"
-              @click="openAll"
-            >
+          class="display-none tablet:display-block tablet:grid-col-5 desktop:grid-col-4 margin-y-2 print:display-none"></div>
+        <div class="tablet:grid-col-4 desktop:grid-col-3 margin-y-2 print:display-none">
+          <div class="display-flex flex-align-center flex-justify-start flex-align-stretch">
+            <button class="usa-button usa-button--unstyled open-all height-5" aria-controls="acc-id" @click="openAll">
               Open All
             </button>
-            /
+            <span class="margin-left-2 height-5">/</span>
             <button
-              class="usa-button usa-button--unstyled close-all"
+              class="margin-left-2 usa-button usa-button--unstyled close-all height-5"
               aria-controls="acc-id"
-              @click="closeAll"
-            >
+              @click="closeAll">
               Close All
             </button>
           </div>
         </div>
-        <div class="grid-col margin-y-2 tablet:text-right">
-          <label
-            role="status"
-            class="usa-label display-inline margin-right-1"
-            for="benefitSort"
-            >Showing {{ lifeEventBenefits.length }} related benefits sorted
-            by:</label
+        <div class="tablet:grid-col margin-y-2 tablet:text-right">
+          <label role="status" class="usa-label display-inline margin-right-1" for="benefitSort"
+            >Showing {{ lifeEventBenefits.length }} related benefits sorted by:</label
           >
           <select
             id="benefitSort"
             class="usa-select margin-left-auto width-card display-inline-block"
             name="options"
-            @change="sortChange"
-          >
-            <option value="relevance" :selected="sort === 'relevance'">
-              Relevance
-            </option>
-            <option value="title" :selected="sort === 'title'">
-              Title (A-Z)
-            </option>
+            @change="sortChange">
+            <option value="relevance" :selected="sort === 'relevance'">Relevance</option>
+            <option value="title" :selected="sort === 'title'">Title (A-Z)</option>
           </select>
         </div>
       </div>
 
       <div class="grid-row grid-gap print:display-block">
         <div
-          class="tablet:grid-col-5 desktop:grid-col-4 desktop:position-sticky desktop:top-1 desktop:height-viewport desktop:overflow-y-auto shade padding-2 radius-md"
-        >
+          class="tablet:grid-col-5 desktop:grid-col-4 desktop:position-sticky desktop:top-1 desktop:height-viewport desktop:overflow-y-auto shade padding-2 radius-md">
           <h2 class="display-none print:display-block">Eligibility criteria</h2>
           <div>
             <div v-if="filter">
               <div class="margin-bottom-3" role="alert">
                 Currently viewing
-                <span
-                  class="usa-tag bg-secondary display-inline-flex margin-left-05 padding-0 usa-button-group__item"
-                >
+                <span class="usa-tag bg-secondary display-inline-flex margin-left-05 padding-0 usa-button-group__item">
                   <button
                     class="usa-tooltip usa-button usa-button--unstyled usa-button--outline usa-button--inverse text-uppercase margin-left-05 border-left border-accent-cool-light padding-x-05 font-sans-3xs"
                     style="padding: 0.25rem; text-decoration: none"
                     :title="`Remove the ${filter} filter`"
                     :aria-label="`Remove the ${filter} filter`"
                     data-position="top"
-                    @click="clearFilter"
-                  >
+                    @click="clearFilter">
                     <span class="text-middle text-white">{{ filter }}</span>
-                    <svg
-                      class="usa-icon text-white text-middle"
-                      aria-hidden="true"
-                      focusable="false"
-                      role="img"
-                    >
+                    <svg class="usa-icon text-white text-middle" aria-hidden="true" focusable="false" role="img">
                       <use xlink:href="~/assets/img/sprite.svg#close" />
                     </svg>
                   </button>
@@ -119,11 +84,8 @@
                   class="usa-icon usa-icon--size-3"
                   aria-labelledby="eligibility-section-criteria-icon-title"
                   focusable="false"
-                  role="img"
-                >
-                  <title id="eligibility-section-criteria-icon-title">
-                    Important information
-                  </title>
+                  role="img">
+                  <title id="eligibility-section-criteria-icon-title">Important information</title>
                   <use xlink:href="~/assets/img/sprite.svg#priority_high" />
                 </svg>
               </div>
@@ -131,44 +93,30 @@
                 {{ lifeEvent.eligibilityCriteriaDescription }}
               </div>
             </div>
-            <CriteriaGroup
-              :life-event-criteria="lifeEvent.eligibilityCriteria"
-            />
+            <CriteriaGroup :life-event-criteria="lifeEvent.eligibilityCriteria" />
             <share-results @print="openAll()" />
           </div>
         </div>
-        <div
-          class="margin-top-2 tablet:margin-top-0 tablet:grid-col-7 desktop:grid-col-8 print:display-block"
-        >
-          <div
-            class="grid-row grid-gap display-none print:display-block break-before-always"
-          >
+        <div class="margin-top-2 tablet:margin-top-0 tablet:grid-col-7 desktop:grid-col-8 print:display-block">
+          <div class="grid-row grid-gap display-none print:display-block break-before-always">
             <div class="grid-col margin-bottom-3">
               <h2 class="display-none print:display-block">Benefits</h2>
-              Showing {{ lifeEventBenefits.length }} related benefits sorted by
-              {{ sort }}.
+              Showing {{ lifeEventBenefits.length }} related benefits sorted by {{ sort }}.
             </div>
           </div>
-          <div
-            v-if="$fetchState.pending"
-            class="usa-alert usa-alert--info usa-alert--no-icon usa-alert--slim"
-          >
+          <div v-if="$fetchState.pending" class="usa-alert usa-alert--info usa-alert--no-icon usa-alert--slim">
             <div class="usa-alert__body">
               <p class="usa-alert__text">Fetching benefits...</p>
             </div>
           </div>
-          <div
-            v-if="$fetchState.error"
-            class="usa-alert usa-alert--error usa-alert--slim"
-          >
+          <div v-if="$fetchState.error" class="usa-alert usa-alert--error usa-alert--slim">
             <div class="usa-alert__body">
               <p class="usa-alert__text">Error while fetching benefits.</p>
             </div>
           </div>
           <div
             v-if="lifeEventBenefits && lifeEventBenefits.length == 0"
-            class="usa-alert usa-alert--error usa-alert--slim"
-          >
+            class="usa-alert usa-alert--error usa-alert--slim">
             <div class="usa-alert__body">
               <p class="usa-alert__text">No matching benefits found.</p>
             </div>
@@ -176,31 +124,29 @@
           <accordion
             ref="accordion"
             :life-event-benefits="lifeEventBenefits"
-            :life-event-criteria="lifeEvent.eligibilityCriteria"
-          />
+            :life-event-criteria="lifeEvent.eligibilityCriteria" />
         </div>
       </div>
     </section>
     <cross-sell
       title="Other benefits that might be relevant to you."
       :cards="lifeEvent.related"
-      class="print:display-none"
-    />
+      class="print:display-none" />
   </div>
 </template>
 
 <script>
-import _ from "lodash";
-import { mapGetters, mapState } from "vuex";
-import mapTags from "~/mixins/MapTags";
+import _ from "lodash"
+import { mapGetters, mapState } from "vuex"
+import mapTags from "~/mixins/MapTags"
 
 export default {
   mixins: [mapTags],
   layout: "default",
   async asyncData({ $content }) {
-    const landingPage = await $content("landing-page").fetch();
+    const landingPage = await $content("landing-page").fetch()
 
-    return { landingPage };
+    return { landingPage }
   },
   data() {
     return {
@@ -218,44 +164,38 @@ export default {
       lifeEventBenefits: [],
       allLifeEventBenefits: [],
       sort: "relevance",
-    };
+    }
   },
 
   async fetch() {
-    const lifeEvent = await this.$content(
-      "life-events",
-      this.$route.params.slug
-    ).fetch();
+    const lifeEvent = await this.$content("life-events", this.$route.params.slug).fetch()
     const lifeEventBenefits = await this.$content("benefits")
       .where({
         lifeEvents: { $contains: this.$route.params.slug },
       })
       .sortBy("title")
-      .fetch();
+      .fetch()
 
-    const allEligibilityCriteria = (await this.$content("criteria").fetch())
-      .body;
-    await this.$store.dispatch("criteria/populate", allEligibilityCriteria);
+    const allEligibilityCriteria = (await this.$content("criteria").fetch()).body
+    await this.$store.dispatch("criteria/populate", allEligibilityCriteria)
 
-    lifeEvent.related = [];
+    lifeEvent.related = []
     for (const related of lifeEvent.relatedKeys || []) {
-      lifeEvent.related.push(
-        await this.$content("life-events", related).fetch()
-      );
+      lifeEvent.related.push(await this.$content("life-events", related).fetch())
     }
 
-    this.lifeEvent = lifeEvent;
-    this.allLifeEventBenefits = this.lifeEventBenefits = lifeEventBenefits;
+    this.lifeEvent = lifeEvent
+    this.allLifeEventBenefits = this.lifeEventBenefits = lifeEventBenefits
   },
   /* istanbul ignore next */
   head() {
     return {
       title: this.lifeEvent.secondaryHeadline,
-    };
+    }
   },
   computed: {
     lifeEventTitle() {
-      return this.lifeEvent.secondaryHeadline;
+      return this.lifeEvent.secondaryHeadline
     },
     ...mapGetters({
       getTotalEligibleCriteria: "criteria/getTotalEligibleCriteria",
@@ -268,17 +208,17 @@ export default {
   watch: {
     eligibilityCriteria: {
       handler(newEligibilityCriteria) {
-        this.sortBenefits();
+        this.sortBenefits()
       },
       deep: true,
     },
   },
   beforeDestroy() {
     /* istanbul ignore next */
-    this.$root.$off("tag:click", this.tagClick);
+    this.$root.$off("tag:click", this.tagClick)
   },
   mounted() {
-    this.$root.$on("tag:click", this.tagClick);
+    this.$root.$on("tag:click", this.tagClick)
   },
   methods: {
     getVirtualCriteria() {
@@ -287,74 +227,62 @@ export default {
           .map((ec) => ec.criteriaKeys)
           .flat()
           .map((criteriaKey) => [criteriaKey, true])
-      );
-      const virtualCriteria = {};
+      )
+      const virtualCriteria = {}
       for (const benefit of this.lifeEventBenefits) {
         for (const criteria of benefit.eligibility) {
           if (!lifeEventCriteria[criteria.criteriaKey]) {
-            virtualCriteria[criteria.criteriaKey] = true;
+            virtualCriteria[criteria.criteriaKey] = true
           }
         }
       }
-      return virtualCriteria;
+      return virtualCriteria
     },
     closeAll() {
-      this.$refs.accordion.closeAll();
+      this.$refs.accordion.closeAll()
     },
     openAll() {
-      this.$refs.accordion.openAll();
+      this.$refs.accordion.openAll()
     },
     sortChange(event) {
-      this.sort = event.target.value;
-      this.sortBenefits();
+      this.sort = event.target.value
+      this.sortBenefits()
     },
     sortBenefits() {
       if (this.sort === "title") {
-        this.lifeEventBenefits = _.sortBy(this.lifeEventBenefits, [this.sort]);
+        this.lifeEventBenefits = _.sortBy(this.lifeEventBenefits, [this.sort])
       } else {
-        const forceToBottom = 2048;
-        const virtualCriteria = this.getVirtualCriteria();
+        const forceToBottom = 2048
+        const virtualCriteria = this.getVirtualCriteria()
         this.lifeEventBenefits.forEach((benefit) => {
-          const matches = (benefit.matches = this.getTotalEligibleCriteria(
-            benefit.eligibility
-          ));
-          const ineligible =
-            this.getTotalIneligibleCriteria(benefit.eligibility) > 0;
-          const virtualBenefitEligibility = (benefit.virtualBenefitEligibility =
-            (benefit.eligibility || []).filter(
-              (c) => virtualCriteria[c.criteriaKey]
-            ).length);
+          const matches = (benefit.matches = this.getTotalEligibleCriteria(benefit.eligibility))
+          const ineligible = this.getTotalIneligibleCriteria(benefit.eligibility) > 0
+          const virtualBenefitEligibility = (benefit.virtualBenefitEligibility = (benefit.eligibility || []).filter(
+            (c) => virtualCriteria[c.criteriaKey]
+          ).length)
           benefit.inverseMatchRatio =
-            1 -
-            matches / (benefit.eligibility.length - virtualBenefitEligibility) +
-            (ineligible ? forceToBottom : 0);
-        });
-        this.lifeEventBenefits = _.sortBy(this.lifeEventBenefits, [
-          "inverseMatchRatio",
-          "title",
-        ]);
+            1 - matches / (benefit.eligibility.length - virtualBenefitEligibility) + (ineligible ? forceToBottom : 0)
+        })
+        this.lifeEventBenefits = _.sortBy(this.lifeEventBenefits, ["inverseMatchRatio", "title"])
       }
     },
     tagClick(tag) {
-      this.lifeEventBenefits = _.filter(
-        this.allLifeEventBenefits,
-        (benefit) => {
-          return benefit.tags.includes(tag);
-        }
-      );
-      this.filter = tag;
-      this.sortBenefits();
+      this.lifeEventBenefits = _.filter(this.allLifeEventBenefits, (benefit) => {
+        return benefit.tags.includes(tag)
+      })
+      this.filter = tag
+      this.sortBenefits()
       // eslint-disable-next-line vue/valid-next-tick
-      setTimeout(() => this.$nextTick(() => this.$refs.accordion.focus()), 250);
+      setTimeout(() => this.$nextTick(() => this.$refs.accordion.focus()), 250)
     },
     clearFilter() {
-      this.filter = "";
-      this.lifeEventBenefits = this.allLifeEventBenefits;
-      this.sortBenefits();
-      this.$nextTick(() => this.$refs.accordion.focus());
+      this.filter = ""
+      this.lifeEventBenefits = this.allLifeEventBenefits
+      this.sortBenefits()
+      this.$nextTick(() => this.$refs.accordion.focus())
     },
   },
-};
+}
 </script>
 
 <style scoped>
