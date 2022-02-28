81b070be833d21152469d7600107e444b0dc8c63
Luke Garceau <lgarceau@uncc.edu>
2022-02-28 16:38:59 -0500

    Criteria Store updates for handling the date

--- store/criteria.js
+++ store/criteria.js
@@ -1,4 +1,5 @@
 import Vue from "vue"
+import { toDate, isEqual, isBefore, isAfter, sub } from "date-fns"
 import stringToHash from "../services/stringToHash"
 
 export const state = () => ({
@@ -35,29 +36,95 @@ export const mutations = {
 }
 
 export const getters = {
+  /**
+   * Function that checks the acceptable criteria date encoding and translates
+   * it to something that the js engine can parse / check
+   * 
+   * Use Cases                                      | Required encoding (acceptable value(s))
+   * - user must be born on MM/DD/YYYY (11/14/1999) | =11-14-1999
+   * - user older than 60 years  (Y, M, D)          | >60Y
+   * - criteria before MM/DD/YYYY (01/01/2022)      | <01-01-2022
+   * - born during YYYY-YYYYY (1990-2000)           | >01-01-1990 , <01-01-2000
+   * - same use case as above (but with age range)  | >60Y, <40Y
+   * @param {currentState} state 
+   * @param {storeGetters} getter 
+   * @returns null / true / false [empty, pass, fail]
+   */
   doesCriterionDateMatch: (state, getters) => (criterion) => {
     if (
       !getters.isCriterionSelected(criterion) || !criterion.acceptableValues
     ) {
       return null
     }
+    let userInputDate = Date.parse(getters.getResponseByEligibilityKey(criterion.criteriaKey))
+    const DETERMINERS = ['months', 'days', 'years']
+    let determiner = null
+    let checkResult = null
     // need to check the date
-    const operator = criterion.acceptableValues[0][0]
-    const acceptDate = Date.parse(criterion.acceptableValues[0].substring(1))
-    const responseDate = Date.parse(getters.getResponseByEligibilityKey(state)(criterion.criteriaKey))
-    if (isNaN(responseDate)) {
-      return null
+    for (const index in criterion.acceptableValues) {
+      const value = criterion.acceptableValues[index].toLowerCase()
+      const operator = value[0]
+      const encodedDate = value.substring(1)
+
+      // date that the users input will be checked against (either the acceptable criteria static
+      //   input or it will be calculated based on the duration in the acceptable criteria)
+      let acceptanceDate = null
+
+      // need to check if there is a determiner in the acceptable value
+      if(DETERMINERS.some((detChar) => {
+        if(encodedDate.includes(detChar)) {
+          determiner = detChar
+          return true
+        }
+        return false
+      })) {
+        const amount = parseInt(value.substring(1, value.indexOf(determiner)))
+        const today = new Date(Date.now())
+        const changeVal = {}
+        changeVal[determiner] = amount
+        acceptanceDate = sub(today, changeVal)
+      } else {
+        acceptanceDate = Date.parse(encodedDate)      
+      }
+      // checking to see if the date from the content file is valid
+      if (isNaN(acceptanceDate)) {
+        checkResult = null
+      } 
+      // checking to see if the inputted date is valid / complete
+      if (!isNaN(userInputDate)) {
+        userInputDate = toDate(userInputDate)
+        switch (operator) {
+          case "=":
+            checkResult = isEqual(userInputDate, acceptanceDate)
+            break
+          case ">":
+            // handling the use case of a user being <60Y & >40Y also being reflected as a range
+            // >01-01-1962, <01-01-1982
+            if(DETERMINERS.includes(determiner)) {              
+              checkResult = isAfter(acceptanceDate, userInputDate)
+            } else {
+              checkResult = isAfter(userInputDate, acceptanceDate)
+            }
+            break
+          case "<":
+            // handling the use case of a user being <60Y & >40Y years also being reflected as a range
+            // >01-01-1962, <01-01-1982
+            if(DETERMINERS.includes(determiner)) {              
+              checkResult = isBefore(acceptanceDate, userInputDate)
+            } else {
+              checkResult = isBefore(userInputDate, acceptanceDate)
+            }
+            break     
+          default:
+            checkResult = null
+            break
+        }
+        if (checkResult === false) {
+          break
+        }
+      }      
     }
-    switch (operator) {
-      case "=":
-        return responseDate === acceptDate
-      case ">":
-        return responseDate > acceptDate
-      case "<":
-        return responseDate < acceptDate
-      default:
-        return null
-    }
+    return checkResult    
   },
   doesCriterionMatchSelection: (state, getters) => (criterion) => {
     if (
