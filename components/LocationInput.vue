7783c5721b92d657a2d1bc8119c61603c6205bdd
Luke Garceau <lgarceau@uncc.edu>
2022-03-01 16:05:10 -0500

    Working on the new data field for zip codes and location (looking at the npm packages zip-cides)

---
+++ components/LocationInput.vue
@@ -0,0 +1,55 @@
+<template>
+
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+    name: 'LocationInput',
+    props: {
+        criteriaKey: {
+            type: String,
+            default: "no criteria key provided"
+        },
+        label: {
+            type: String,
+            default: "no label provided"
+        },
+        response: {
+            type: [String, Object, Boolean],
+            default: "no response provided"
+        },
+        location: {
+            type: String,
+            validator: (value) => {
+                return ['benefit-card', 'left-rail'].includes(value)
+            },
+            default: 'benefit-card'
+        }
+    },
+    data() {
+        return {
+            uniqueId: _.uniqueId('locationinput-')
+        }
+    },
+    computed: {
+        selectedStyle() {
+            if (this.location === "benefit-card" && this.response === true) {
+                return "text-success text-bold"
+            }
+            return null
+        }
+    },
+    mounted() {
+        this.uniqueId = _.uniqueId("checkbox-")
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
