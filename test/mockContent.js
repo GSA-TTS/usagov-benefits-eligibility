8dfbb11a93a4e34caa416c5091f4f2ca88f401da
jschoenemann-cms <jschoenemann-cms@users.noreply.github.com>
2022-03-08 22:10:29 +0000

    [MegaLinter] Apply linters fixes

--- test/mockContent.js
+++ test/mockContent.js
@@ -1,28 +1,35 @@
-import Loki from '@lokidb/loki';
-import QueryBuilder from '@nuxt/content/lib/query-builder';
+import Loki from "@lokidb/loki"
+import QueryBuilder from "@nuxt/content/lib/query-builder"
 
 // Mocks nuxt/content's $content instance for use with static mock data
 // See https://content.nuxtjs.org/fetching for related docs
 
-export function createContentMock (collections) {
-  const db = new Loki("content.db");
+export function createContentMock(collections) {
+  const db = new Loki("content.db")
   collections.forEach(({ collectionName, items }) => {
-    const mockCollection = db.addCollection(collectionName);
-    mockCollection.insert([...items]);
-  });
+    const mockCollection = db.addCollection(collectionName)
+    mockCollection.insert([...items])
+  })
   // this mocks $content  a filter by dir returns an array,
   // if specific content is added as an optional param, a single object is returned
   return jest.fn(function (queryName, options = null) {
-    if(options == null){
-      const query = new QueryBuilder({
-        query: db.getCollection(queryName).chain(),
-        path: queryName,
-        text: false,
-      }, {})
-      return query;
-    }else {
-      const obj = db.getCollection(queryName).findOne({slug:options})
-      return { "fetch"() { return obj}}
+    if (options == null) {
+      const query = new QueryBuilder(
+        {
+          query: db.getCollection(queryName).chain(),
+          path: queryName,
+          text: false,
+        },
+        {}
+      )
+      return query
+    } else {
+      const obj = db.getCollection(queryName).findOne({ slug: options })
+      return {
+        fetch() {
+          return obj
+        },
+      }
     }
-  });
+  })
 }
