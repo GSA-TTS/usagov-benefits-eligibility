121686236cb518563b4efcff8f24f8faf00a92d7
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-04-15 16:18:10 -0500

    starts usagov header footer with flag in layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
-    <Header />
+    <UsaGovHeader v-if="isUsaGov" />
+    <Header v-else />
     <main id="main-content">
       <Nuxt />
     </main>
-    <Footer />
+    <UsaGovFooter v-if="isUsaGov" />
+    <Footer v-else />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isUsaGov: true,
+    }
+  },
+}
+</script>
