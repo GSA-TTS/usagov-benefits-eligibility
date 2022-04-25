19f4f9512d6c8095bd3291541e15a64bd11bdcf5
Luke Garceau <luke.garceau@gsa.gov>
2022-04-25 13:06:56 -0400

    Update to #164

--- components/DateInput.vue
+++ components/DateInput.vue
@@ -144,9 +144,11 @@ export default {
   },
   watch: {
     dateResponse() {
-      this.month = this.pullDateValue(this.dateResponse, 0)
-      this.day = this.pullDateValue(this.dateResponse, 1)
-      this.year = this.pullDateValue(this.dateResponse, 2)
+      if (this.check === "") {
+        this.month = this.pullDateValue(this.dateResponse, 0)
+        this.day = this.pullDateValue(this.dateResponse, 1)
+        this.year = this.pullDateValue(this.dateResponse, 2)
+      }
     },
   },
   data() {
