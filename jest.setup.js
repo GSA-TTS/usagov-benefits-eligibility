5cb193796af5c73d88e3ed60aeb4bbdf476fda1e
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-05-09 17:23:15 -0500

    cleanup

--- jest.setup.js
+++ jest.setup.js
@@ -4,13 +4,9 @@ import nuxtConfig from "@/nuxt.config.js"
 const locale = nuxtConfig.i18n.defaultLocale
 const translations = nuxtConfig.i18n.vueI18n.messages
 
-config.mocks["$i18n"] = {
-  locale: locale,
-}
-config.mocks["i18n"] = {
-  locale: locale,
-}
+config.mocks.$i18n = { locale }
+config.mocks.i18n = { locale }
 
 config.mocks.$t = (msg) => translations[locale][msg]
 config.mocks.switchLocalePath = (msg) => msg
-config.mocks.localePath = (i) => i``
+config.mocks.localePath = (i) => i
