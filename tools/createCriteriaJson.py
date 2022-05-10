5ccdd490ef1d5046965d689e8a38d740859d5226
lgarceau768 <lgarceau768@users.noreply.github.com>
2022-05-10 18:56:46 +0000

    [MegaLinter] Apply linters fixes

--- tools/createCriteriaJson.py
+++ tools/createCriteriaJson.py
@@ -1,38 +1,40 @@
-import os, csv, json
+import csv
+import json
+import os
 
-criteriaFile = '../content/criteria.csv'
+criteriaFile = "../content/criteria.csv"
 
 translatedJson = {}
 lines = []
-with open(criteriaFile, mode='r') as file:
-    criteriaFile = csv.reader(file, delimiter=',')
+with open(criteriaFile, mode="r") as file:
+    criteriaFile = csv.reader(file, delimiter=",")
     for row in criteriaFile:
-        
+
         # column 1 is the label
         label = row[1]
-        key = 'criteria.'+row[0]
+        key = "criteria." + row[0]
         row[1] = key
         translatedJson[key] = label
-        
+
         # translate the options in the csv
-        if row [2] == 'select' or row[2] == 'radio':
-            options = row[3].split(';')
+        if row[2] == "select" or row[2] == "radio":
+            options = row[3].split(";")
             i = 0
-            newString = ''
+            newString = ""
             for val in options:
-                jsonKey = 'criteria.'+key+'.values'+str(i)
+                jsonKey = "criteria." + key + ".values" + str(i)
                 translatedJson[jsonKey] = val
-                i+=1
-                newString = jsonKey + ';' + newString
+                i += 1
+                newString = jsonKey + ";" + newString
             row[3] = newString
         lines.append(row)
     file.close()
-    
-outputFile = '../locales/en/criteria.json'
-with open(outputFile, mode='w') as file:
+
+outputFile = "../locales/en/criteria.json"
+with open(outputFile, mode="w") as file:
     file.write(json.dumps(translatedJson, indent=4))
     file.close()
-    
-with open(criteriaFile, mode='w') as file:
+
+with open(criteriaFile, mode="w") as file:
     file.writelines(lines)
     file.close()
