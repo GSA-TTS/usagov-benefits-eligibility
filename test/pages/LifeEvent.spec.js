98834bcd0886d4bdc0c4d9d41cfe7b1be0c5ab2e
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2022-06-24 13:55:11 -0500

    Update LifeEvent.spec.js
    fixes tests based on:
    https://stackoverflow.com/questions/39830580/jest-test-fails-typeerror-window-matchmedia-is-not-a-function

--- test/pages/LifeEvent.spec.js
+++ test/pages/LifeEvent.spec.js
@@ -101,6 +101,19 @@ describe("Life Event page", () => {
   let store
 
   beforeAll(async () => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    })
     config.mocks.$config = {
       oneEventVersion: false,
     }
