89afc059adb1874c8806b19602cc3c9412001a82
Luke Garceau <luke.garceau@gsa.gov>
2022-07-21 13:13:25 -0400

    Split up the variable and documented better

--- pages/_lifeEvent.vue
+++ pages/_lifeEvent.vue
@@ -342,9 +342,14 @@ export default {
         const forceToBottom = 2048
         this.lifeEventBenefits.forEach((benefit) => {
           const matches = (benefit.matches = this.getTotalEligibleCriteria(benefit.eligibility))
-          const ineligible = this.getTotalIneligibleCriteria(benefit.eligibility) > 0          
+          const ineligible = this.getTotalIneligibleCriteria(benefit.eligibility) > 0 
+          const nonMatch = (ineligible ? forceToBottom : 0)      
+          // takes into account the amount of possible criteria there is too match vs the
+          // amount matched and then grades it in accordance (the 1 - is to invert the list)
+          // low the score the better. The last non match is used to force the benefit to 
+          // the bottom if there is any non matching criteria
           benefit.inverseMatchRatio = 
-           1 - (matches / benefit.eligibility.length) + (ineligible ? forceToBottom : 0)
+           1 - (matches / benefit.eligibility.length) + nonMatch
         })
         this.matchingBenefitMessage = `${this.lifeEventBenefits.length} matching benefits`
         this.lifeEventBenefits = _.sortBy(this.lifeEventBenefits, ["inverseMatchRatio", "title"])
