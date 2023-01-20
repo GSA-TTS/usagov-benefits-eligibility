613db2f4681b65f9394f0fb9706cd219f53807b6
lgarceau768 <lgarceau768@users.noreply.github.com>
2023-01-20 15:42:06 +0000

    [MegaLinter] Apply linters fixes

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -1,6 +1,6 @@
 export default function ({ app }) {
-    // onBeforeLanguageSwitch called right before setting a new locale
-    app.i18n.onBeforeLanguageSwitch = (oldLocale, newLocale, isInitialSetup, context) => {
-        app.store.dispatch("criteria/clear")
-    }
+  // onBeforeLanguageSwitch called right before setting a new locale
+  app.i18n.onBeforeLanguageSwitch = (oldLocale, newLocale, isInitialSetup, context) => {
+    app.store.dispatch("criteria/clear")
+  }
 }
