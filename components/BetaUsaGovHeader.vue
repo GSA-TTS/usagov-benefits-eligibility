59b91168a5ed429aeddd4f59a858815f899aaff0
fpigeonjr <frank.pigeonjr@gmail.com>
2023-01-24 16:59:20 -0600

    wip
    - adds language toggle
    - mobile styles
    - adds icons
    - adds alert

--- components/BetaUsaGovHeader.vue
+++ components/BetaUsaGovHeader.vue
@@ -1,30 +1,42 @@
 <template>
-  <header
-    id="header"
-    class="usa-header usa-header--extended">
-    <div class="usa-banner-inner">
-      <div
-        id="block-languageswitcher"
-        class="language-switcher-language-url settings-tray-editable block block-language block-language-blocklanguage-interface"
-        role="navigation"
-        data-drupal-settingstray="editable">
-        <ul class="links usa-nav__primary usa-accordion">
-          <li
-            hreflang="es"
-            class="usa-nav__primary-item">
-            <a
-              href="/es"
-              class="language-link"
-              hreflang="es"
-              >Español</a
-            >
-          </li>
-        </ul>
+  <div>
+    <a
+      class="usa-skipnav"
+      href="#main-content"
+      @click.prevent="skipLink"
+      >{{ $t("skipnav") }}</a
+    >
+    <TheBanner />
+    <div>
+      <div class="usa-alert usa-alert--slim usa-alert--info">
+        <div class="usa-alert__body">
+          <strong>This is a beta site.</strong> It's an early, in-progress version of an improved
+          <a href="https://www.usa.gov/">USA.gov</a>. If you don't find what you're looking for, return to the main
+          site.
+        </div>
       </div>
-      <div
-        id="block-englishheadercode"
-        class="settings-tray-editable block block-block-content block-block-content644b7adb-94b1-497d-bd4d-c1f2a2bf9909"
-        data-drupal-settingstray="editable">
+    </div>
+    <header
+      id="header"
+      class="usa-header usa-header--extended">
+      <div class="usa-banner-inner">
+        <div
+          class="language-switcher-language-url"
+          role="navigation">
+          <ul class="links usa-nav__primary usa-accordion flex-justify-end">
+            <li
+              hreflang="es"
+              class="usa-nav__primary-item">
+              <button
+                class="language-link usa-button"
+                hreflang="es"
+                @click="switchLanguage">
+                Español
+              </button>
+            </li>
+          </ul>
+        </div>
+
         <div class="field field--name-body field--type-text-with-summary field--label-hidden field__item">
           <div class="usa-navbar">
             <div
@@ -38,7 +50,7 @@
                     v-if="$i18n.locale === 'en'"
                     src="~/assets/img/logo-beta.png"
                     alt="USAGov Logo"
-                    class="maxw-10" />
+                    class="maxw-10 logo" />
                   <img
                     v-if="$i18n.locale === 'es'"
                     src="~/assets/img/logo-beta-es.png"
@@ -55,7 +67,7 @@
             <div class="usa-nav__inner">
               <button class="usa-nav__close">
                 <img
-                  src="/themes/custom/usagov/assets/img/usa-icons/close.svg"
+                  src="@/assets/img/usa-icons/close.svg"
                   alt="Close" />
               </button>
               <ul class="usa-nav__primary usa-accordion">
@@ -108,7 +120,7 @@
                   <span
                     id="top-phone"
                     class="usa-nav__secondary-item">
-                    <a href="/phone">Call us at 1-844-USAGOV1</a>
+                    <a href="https://beta.usa.gov/phone">Call us at 1-844-USAGOV1</a>
                   </span>
                 </span>
 
@@ -144,7 +156,7 @@
                     class="usa-button"
                     type="submit">
                     <img
-                      src="/themes/custom/usagov/images/search--dark.svg"
+                      src="@/assets/img/search-dark.svg"
                       class="usa-search__submit-icon"
                       alt="Search" />
                   </button>
@@ -154,8 +166,8 @@
           </nav>
         </div>
       </div>
-    </div>
-  </header>
+    </header>
+  </div>
 </template>
 
 <script>
@@ -200,5 +212,106 @@ export default {
 <style lang="scss" scoped>
 .maxw-196 {
   max-width: 12.3rem;
+}
+.usa-banner {
+  background-color: #f3f3f3;
+}
+.language-switcher-language-url {
+  margin-right: auto;
+  max-width: 64rem;
+  margin-left: auto;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  display: block;
+
+  ul.links {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 0.3rem;
+    margin-left: 0;
+  }
+}
+.language-link {
+  color: #fff;
+  text-decoration: none;
+  padding: 10px 20px;
+  background-color: #112f4e;
+  &:hover {
+    color: #fff;
+    background-color: #112f4e;
+  }
+}
+
+.usa-header--extended [hreflang|="es"] {
+  border-radius: 5px;
+  border: 0;
+}
+.usa-nav__secondary {
+  flex-direction: row;
+  align-items: flex-end;
+  bottom: 6rem;
+  width: 32rem;
+}
+
+.usa-nav__secondary-links {
+  width: 15rem;
+  line-height: 1.1rem;
+  a {
+    color: #000;
+    display: inline-block;
+    text-decoration: none;
+    font-size: 1.13rem;
+    padding-left: 0.5rem;
+    padding-top: 0.2rem;
+    &:hover {
+      text-decoration: revert;
+    }
+  }
+}
+
+.usa-nav__secondary-item::after {
+  float: right;
+  color: #dfe1e2;
+  content: "|";
+  padding-right: 1rem;
+  font-size: 2rem;
+  padding-bottom: 0.4rem;
+}
+
+.usa-logo {
+  margin: 0.5rem 0 0.2em;
+}
+
+// alert
+.usa-alert__body {
+  max-width: 64rem;
+  margin-right: auto;
+  margin-left: auto;
+  padding-left: 3.2rem;
+  padding-right: 3.2rem;
+  display: block;
+  font-size: 0.9rem;
+  position: relative;
+}
+.usa-alert--info {
+  border-left-color: #e7f6f8;
+  &::before {
+    background: url(~/assets/img/usa-icons/info.svg) no-repeat center/2rem 2rem;
+    width: 2rem;
+    position: absolute;
+    left: 257px;
+    top: -2px;
+    background-color: #1b1b1b;
+  }
+}
+
+@media only screen and (max-width: 64em) {
+  .usa-header--extended .usa-navbar {
+    margin-top: 1rem;
+    padding-bottom: 1rem;
+  }
+  .logo {
+    max-width: 2.5rem;
+  }
 }
 </style>
