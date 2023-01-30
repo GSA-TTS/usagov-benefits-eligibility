e3da53507b297833e738b3b5468f7ce0c0902be1
Renata Bartlett <66442872+r-bartlett-gsa@users.noreply.github.com>
2023-01-30 16:15:37 -0600

    Added new benefit file

--- locales/en.js
+++ locales/en.js
@@ -81,6 +81,7 @@ export default Object.assign(
   require("./en/benefits/doi-housing-improvement-program.json"),
   require("./en/benefits/doi-financial-assistance-social-services.json"),
   require("./en/benefits/hhs-temporary-assistance-for-needy-families.json"),
+  require("./en/benefits/doj-public-safety-officers-disability-benefits.json"),
   require("./en/life-events/becoming-a-parent.json"),
   require("./en/life-events/military-service.json"),
   require("./en/life-events/national-emergency-and-disaster.json"),
