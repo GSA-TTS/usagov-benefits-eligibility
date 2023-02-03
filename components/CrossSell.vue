1bb897b915476f422e80ea516bd01c2e7e3da915
danny-englander <dannyenglander311@gmail.com>
2023-02-03 10:23:54 -0500

    fixing bottom card area html

--- components/CrossSell.vue
+++ components/CrossSell.vue
@@ -10,29 +10,30 @@
           {{ title }}
         </h1>
       </div>
-      <div class="grid-row grid-gap">
-        <div class="tablet:grid-col">
-          <ul
-            v-if="cards && cards.length"
-            class="usa-card-group">
-            <li
-              v-for="card in cards"
-              :key="card.slug"
-              class="usa-card desktop:grid-col-6"
-              :aria-label="card.title">
-              <nuxt-link
-                class="display-block height-full margin-x-1"
-                style="text-decoration: none; outline-offset: 0.25rem"
-                :to="localePath(`/${card.slug}`)">
-                <Card
-                  :card-body="card.summary"
-                  :card-title="card.title"
-                  :card-container-classes="['hover:border-base-light', 'margin-x-0']"
-                  card-title-heading-level="h2" />
-              </nuxt-link>
-            </li>
-          </ul>
-        </div>
+    </div>
+
+    <div class="grid-row grid-gap">
+      <div class="tablet:grid-col">
+        <ul
+          v-if="cards && cards.length"
+          class="usa-card-group">
+          <li
+            v-for="card in cards"
+            :key="card.slug"
+            class="usa-card desktop:grid-col-6"
+            :aria-label="card.title">
+            <nuxt-link
+              class="display-block height-full margin-x-1"
+              style="text-decoration: none; outline-offset: 0.25rem"
+              :to="localePath(`/${card.slug}`)">
+              <Card
+                :card-body="card.summary"
+                :card-title="card.title"
+                :card-container-classes="['hover:border-base-light', 'margin-x-0']"
+                card-title-heading-level="h2" />
+            </nuxt-link>
+          </li>
+        </ul>
       </div>
     </div>
   </section>
