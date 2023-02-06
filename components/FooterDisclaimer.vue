707e3380c96c0ee4e2218bcff6cd6973e310a26c
danny-englander <dannyenglander311@gmail.com>
2023-02-06 15:24:59 -0500

    fixes #630

--- components/FooterDisclaimer.vue
+++ components/FooterDisclaimer.vue
@@ -11,15 +11,15 @@
               <h2
                 id="summary-box-key-information-disclaimer"
                 class="usa-summary-box__heading">
-                {{ disclaimer.heading }}
+                {{ $t("disclaimer.heading") }}
               </h2>
               <div class="usa-summary-box__text">
                 <ul class="usa-list">
                   <li style="max-width: unset">
-                    {{ disclaimer.bullet }}
+                    {{ $t("disclaimer.bullet") }}
                   </li>
                   <li style="max-width: unset">
-                    {{ disclaimer.bullet1 }}
+                    {{ $t("disclaimer.bullet1") }}
                   </li>
                 </ul>
               </div>
