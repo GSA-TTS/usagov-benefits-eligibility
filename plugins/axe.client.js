337e2125b386b297b5702e1eb1780a8c94a50680
danny-englander <dannyenglander311@gmail.com>
2023-02-15 08:22:19 -0500

    #659 - potential fix for console.log

--- plugins/axe.client.js
+++ plugins/axe.client.js
@@ -3,6 +3,8 @@ import Vue from "vue"
 export default function () {
   if (process.env.NODE_ENV === "development") {
     const VueAxe = require("vue-axe").default
-    Vue.use(VueAxe)
+    Vue.use(VueAxe, {
+      allowConsoleClears: false, // disable all console clears
+    })
   }
 }
