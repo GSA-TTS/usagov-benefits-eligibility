265623874ac701cd2c1622c2beba04c27a77e5c8
danny-englander <dannyenglander311@gmail.com>
2023-02-16 12:54:33 -0500

    #582 hopefully fixing social icons

--- components/BetaUsaGovFooterSocial.vue
+++ components/BetaUsaGovFooterSocial.vue
@@ -4,7 +4,6 @@
     <h2 class="usa-footer__contact-heading">{{ $t("beta.footer.social.title") }}</h2>
     <div class="usa-footer__social-links en-links grid-row grid-gap-1">
       <!-- Footer social links loop. -->
-
       <div
         v-for="item in $t('beta.footer.social.icons')"
         :key="`social-id-${item.socialID}`"
@@ -12,6 +11,11 @@
         <a
           class="usa-social-link"
           :href="sanitizedBearsUrl(item.socialURL)">
+          <img
+            :src="require(`@/assets/img-custom/${item.icon}.svg`)"
+            :alt="item.name"
+            :width="item.width"
+            :height="item.height" />
           <span class="is-hidden">{{ item.name }}</span>
         </a>
       </div>
