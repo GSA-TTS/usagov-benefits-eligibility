7adb75927c952123a1d179afab7eb84e6fbdd79b
gchi25 <Yuechen.Chi@gsa.gov>
2023-04-26 12:20:34 -0400

    #748 change coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -20,10 +20,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 85,
-      functions: 85,
-      lines: 90,
-      statements: 90,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
   setupFiles: ["<rootDir>/jest.setup.js"],
