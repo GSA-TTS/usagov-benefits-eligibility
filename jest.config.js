29b123131680bb2d77a8a82fd2f429910eacd529
gscottqueen <scott.queen@contractor.bixal.com>
2023-04-26 15:23:29 -0400

    moves tranform to transform object

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,13 @@ module.exports = {
     "^@/(.*)$": "<rootDir>/$1",
     "^~/(.*)$": "<rootDir>/$1",
     "^vue$": "vue/dist/vue.common.js",
-    ".+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
   },
   moduleFileExtensions: ["js", "vue", "json"],
 
   transform: {
     "^.+\\.js$": "babel-jest",
     ".*\\.(vue)$": "vue-jest",
+    ".+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
   },
   collectCoverage: true,
   collectCoverageFrom: [
