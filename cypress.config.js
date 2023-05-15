a79a70b30add34c5e19b3ebca8e74734bb4356fc
FatmaBakir <FatmaBakir@users.noreply.github.com>
2023-05-15 20:29:35 +0000

    [MegaLinter] Apply linters fixes

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,23 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   reporter: "cypress-multi-reporters",
   reporterOptions: {
-    "configFile": "reporter-config.json"
+    configFile: "reporter-config.json",
   },
-  "video": false,
-  "retries": {
-    "runMode": 1,
-    "openMode": 1
+  video: false,
+  retries: {
+    runMode: 1,
+    openMode: 1,
   },
   e2e: {
-    baseUrl: 'http://localhost:3000/',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    baseUrl: "http://localhost:3000/",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
     testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
