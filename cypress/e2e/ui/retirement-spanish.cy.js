b0446a1e96aceb8d351357ac7f48fecb50a24013
nehemiah-abuga <nehemiah-abuga@users.noreply.github.com>
2023-06-12 13:20:27 +0000

    [MegaLinter] Apply linters fixes

--- cypress/e2e/ui/retirement-spanish.cy.js
+++ cypress/e2e/ui/retirement-spanish.cy.js
@@ -1,12 +1,11 @@
 /// <reference types="cypress"/>
 
-describe("Retirement Spanish page tests", ()=> {
-   
-    it("Verify UI is rendering content correctly instead of dot notation value on checkbox labels", ()=> {
-        cy.visit('es/retirement')
-        cy.get(".usa-checkbox__label").each(checkboxLabels => {
-            cy.wrap(checkboxLabels).invoke('prop', 'innerText').should('not.contain', 'criteria.')
-            cy.wrap(checkboxLabels).invoke('prop', 'innerText').should('not.contain', '.label')
-        })
+describe("Retirement Spanish page tests", () => {
+  it("Verify UI is rendering content correctly instead of dot notation value on checkbox labels", () => {
+    cy.visit("es/retirement")
+    cy.get(".usa-checkbox__label").each((checkboxLabels) => {
+      cy.wrap(checkboxLabels).invoke("prop", "innerText").should("not.contain", "criteria.")
+      cy.wrap(checkboxLabels).invoke("prop", "innerText").should("not.contain", ".label")
     })
+  })
 })
