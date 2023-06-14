55751bf6c369fae8d3030aff8a337ea95c31b2d0
nehemiah-abuga <nehemiah-abuga@users.noreply.github.com>
2023-06-14 17:17:45 +0000

    [MegaLinter] Apply linters fixes

--- cypress/e2e/ui/links.cy.js
+++ cypress/e2e/ui/links.cy.js
@@ -6,14 +6,14 @@ describe("Verify status code 200 when user navigates links", () => {
   sitePages.forEach((sitePage) => {
     it(`Verify success 200 response for links in ${sitePage.name} `, () => {
       cy.visit({ url: sitePage.route })
-      cy.get("a[href]").each(link => {
+      cy.get("a[href]").each((link) => {
         cy.request({
-          url: link.prop('href'),
-          failOnStatusCode: false
+          url: link.prop("href"),
+          failOnStatusCode: false,
         }).then((response) => {
           expect(response.status).to.eq(200)
         })
-    })
+      })
     })
   })
 })
