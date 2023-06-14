721d686794a6e359a83974b9fc1703a1262d9d51
Nehemiah Abuga <nehemiah.abuga@bixal.com>
2023-06-14 09:59:36 -0700

    add links tests

---
+++ cypress/e2e/ui/links.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+const sitePages = require("../../fixtures/site-pages.json")
+
+describe("Verify status code 200 when user navigates links", () => {
+  sitePages.forEach((sitePage) => {
+    it(`Verify success 200 response for links in ${sitePage.name} `, () => {
+      cy.visit({ url: sitePage.route })
+      cy.get("a[href]").each(link => {
+        cy.request({
+          url: link.prop('href'),
+          failOnStatusCode: false
+        }).then((response) => {
+          expect(response.status).to.eq(200)
+        })
+    })
+    })
+  })
+})
