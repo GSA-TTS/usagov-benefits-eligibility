4fa6a3af4b6ea22b831fbd343b2011caf598ba62
nehemiah-abuga <nehemiah-abuga@users.noreply.github.com>
2023-06-22 02:10:09 +0000

    [MegaLinter] Apply linters fixes

--- cypress/e2e/ui/open-all-close-all-button.cy.js
+++ cypress/e2e/ui/open-all-close-all-button.cy.js
@@ -3,28 +3,27 @@
 import { pages } from "../../support/page-objects/pages.js"
 const sitePages = require("../../fixtures/site-pages.json")
 
-describe('Validate Open All button functionality', ()=> {
-    sitePages.forEach(sitePage => {
-        it(`Verify Open All button should open all accordion cards in ${sitePage.name}`, ()=> {
-            cy.visit({ url: sitePage.route })
-            pages.openAllButton().click()
-            pages.accordions().each((accordion) => {
-                cy.wrap(accordion).invoke("prop", "ariaExpanded").should('eq', 'true')
-            })
-        })
-    });  
+describe("Validate Open All button functionality", () => {
+  sitePages.forEach((sitePage) => {
+    it(`Verify Open All button should open all accordion cards in ${sitePage.name}`, () => {
+      cy.visit({ url: sitePage.route })
+      pages.openAllButton().click()
+      pages.accordions().each((accordion) => {
+        cy.wrap(accordion).invoke("prop", "ariaExpanded").should("eq", "true")
+      })
+    })
+  })
 })
 
-describe('Validate Close All button functionality', ()=> {
-    sitePages.forEach(sitePage => {
-        it(`Verify Close All button should close all accordion cards in ${sitePage.name}`, ()=> {
-            cy.visit({ url: sitePage.route })
-            pages.openAllButton().click()
-            pages.closeAllButton().click()
-            pages.accordions().each((accordion) => {
-                cy.wrap(accordion).invoke("prop", "ariaExpanded").should('eq', 'false')
-            })
-        })
-        
-    });  
+describe("Validate Close All button functionality", () => {
+  sitePages.forEach((sitePage) => {
+    it(`Verify Close All button should close all accordion cards in ${sitePage.name}`, () => {
+      cy.visit({ url: sitePage.route })
+      pages.openAllButton().click()
+      pages.closeAllButton().click()
+      pages.accordions().each((accordion) => {
+        cy.wrap(accordion).invoke("prop", "ariaExpanded").should("eq", "false")
+      })
+    })
+  })
 })
