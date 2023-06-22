8b2d21f8684632589f1624287318bd9aadb7a958
Nehemiah Abuga <nehemiah.abuga@bixal.com>
2023-06-21 18:59:04 -0700

    made changes to utilize page objects

--- cypress/e2e/ui/open-all-close-all-button.cy.js
+++ cypress/e2e/ui/open-all-close-all-button.cy.js
@@ -1,13 +1,14 @@
 /// <reference types="cypress"/>
 
+import { pages } from "../../support/page-objects/pages.js"
 const sitePages = require("../../fixtures/site-pages.json")
 
 describe('Validate Open All button functionality', ()=> {
     sitePages.forEach(sitePage => {
         it(`Verify Open All button should open all accordion cards in ${sitePage.name}`, ()=> {
             cy.visit({ url: sitePage.route })
-            cy.get('.flex-row .open-all').click()
-            cy.get('.usa-accordion__heading .usa-accordion__button').each((accordion) => {
+            pages.openAllButton().click()
+            pages.accordions().each((accordion) => {
                 cy.wrap(accordion).invoke("prop", "ariaExpanded").should('eq', 'true')
             })
         })
@@ -18,9 +19,9 @@ describe('Validate Close All button functionality', ()=> {
     sitePages.forEach(sitePage => {
         it(`Verify Close All button should close all accordion cards in ${sitePage.name}`, ()=> {
             cy.visit({ url: sitePage.route })
-            cy.get('.flex-row .open-all').click()
-            cy.get('.flex-row .close-all').click()
-            cy.get('.usa-accordion__heading .usa-accordion__button').each((accordion) => {
+            pages.openAllButton().click()
+            pages.closeAllButton().click()
+            pages.accordions().each((accordion) => {
                 cy.wrap(accordion).invoke("prop", "ariaExpanded").should('eq', 'false')
             })
         })
--- cypress/e2e/ui/ui-render-content.cy.js
+++ cypress/e2e/ui/ui-render-content.cy.js
@@ -1,14 +1,15 @@
 /// <reference types="cypress"/>
 
+import { pages } from "../../support/page-objects/pages.js"
 const sitePages = require("../../fixtures/site-pages.json")
 
 describe("Verify UI is rendering content correctly", () => {
   sitePages.forEach((sitePage) => {
     it(`Verify ${sitePage.name} is rendering content correctly instead of dot notation value on checkbox labels`, () => {
       cy.visit({ url: sitePage.route })
-      cy.get(".usa-checkbox__label").each((checkboxLabels) => {
-        cy.wrap(checkboxLabels).invoke("prop", "innerText").should("not.contain", "criteria.")
-        cy.wrap(checkboxLabels).invoke("prop", "innerText").should("not.contain", ".label")
+      pages.checkboxLabels().each((checkboxLabel) => {
+        cy.wrap(checkboxLabel).invoke("prop", "innerText").should("not.contain", "criteria.")
+        cy.wrap(checkboxLabel).invoke("prop", "innerText").should("not.contain", ".label")
       })
     })
   })
