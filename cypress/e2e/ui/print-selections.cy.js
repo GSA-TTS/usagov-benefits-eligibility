5a07fe96ed92890e7b01527341778cb4dc6e0866
Nehemiah Abuga <nehemiah.abuga@bixal.com>
2023-06-27 07:57:49 -0700

    add test that stub print my selections

---
+++ cypress/e2e/ui/print-selections.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+import { pages } from "../../support/page-objects/pages.js"
+import * as EN_CRITERIA from "../../../locales/en/criteria.json"
+
+describe("Select ", () => {
+    before(() => {
+        cy.visit("/")
+      })
+
+      it("Select a criteria and stub printing when user clicks Print My Selections", () => {
+        pages.checkboxLabels().contains(EN_CRITERIA["criteria.deceased_death_location_is_US.label"]).click()
+        cy.window().then(win => {
+            cy.stub(win, 'print').as('print')
+          });
+        pages.buttons().contains("Print my selections").click()
+         cy.get('@print').should('be.calledOnce')
+      })
+})
